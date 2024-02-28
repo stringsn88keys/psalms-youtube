4f09b7c4d23141496cbc0598d56cfdab9d44fc75
Thomas Powell <twilliampowell@gmail.com>
2024-02-28 10:05:22 -0500

    Wrapping up initial implementation
    Signed-off-by: Thomas Powell <twilliampowell@gmail.com>

--- generate.rb
+++ generate.rb
@@ -22,13 +22,50 @@ end
 
 using StringHelpers
 
+def author(book, part, week)
+  book['author'] || week[part.snake_case]['alt'][book['short_title']]['author']
+end
+
+def title(book, part, week)
+  case part
+  when /Psalm/
+    week[part.snake_case]['title']
+  when /Gospel/
+    week[part.snake_case]['alt'][book['short_title']]['response']
+  end
+end
+
+def sub_description(book, part, week)
+  lines = []
+  if week['liturgical_week'] =~ / of /
+    lines << "#{book['title']} #{part} for the #{week['liturgical_week']}"
+  else
+    lines << "#{book['title']} #{part} for #{week['liturgical_week']}"
+  end
+
+  if part =~ /Gospel/
+    lines << "#{week['gospel_acclamation']['verse']}"
+  end
+  lines.join("\n")
+end
+
+def extra
+  [
+  "Get the Psalm Recording links on Tuesdays by becoming a patron on Patreon patreon.com/ThomasPowell88Keys",
+  "Yamaha #clavinova CLP-585 CFX Grand"
+  ].join("\n")
+end
+
 data['Weeks'].each do |week|
   File.open(week['liturgical_week_short']+"descriptions.txt", "wt+") do |f|
+    puts "=== #{week['liturgical_week']} ==="
     data['Books'].each do |book|
       book['parts'].each do |part|
-        puts "Filename: #{book['short_title']}_#{week['liturgical_week_short']}_#{book['short_publisher']}_#{short_part(part)}"
-        puts part.snake_case
-        puts "\"#{week[part.snake_case]['title']}\" - #{author(book, week)}"
+        f.puts "Filename: #{book['short_title']}_#{week['liturgical_week_short']}_#{book['short_publisher']}_#{short_part(part)}"
+        f.puts "\"#{title(book, part, week)}\" - #{author(book, part, week)}"
+        f.puts sub_description(book, part, week)
+        f.puts extra
+        f.puts
       end
     end
   end
