67235ca3fcbc1b4a922d73b3cbc113e19bff00cf
Thomas Powell <twilliampowell@gmail.com>
2024-03-18 23:02:50 -0400

    Some broken structure around an extra part
    Signed-off-by: Thomas Powell <twilliampowell@gmail.com>

--- generate.rb
+++ generate.rb
@@ -22,33 +22,33 @@ end
 
 using StringHelpers
 
-def author(book, part, week)
-  book['author'] || week[part.snake_case]['alt'][book['short_title']]['author']
+def author(book, part, liturgy)
+  book['author'] || liturgy[part.snake_case]['alt'][book['short_title']]['author']
 end
 
-def title(book, part, week)
+def title(book, part, liturgy)
   case part
   when /Psalm/
-    if week[part.snake_case]['alt'][book['short_title']]&.[]('response')
-      week[part.snake_case]['alt'][book['short_title']]['response']
+    if liturgy[part.snake_case]['alt'][book['short_title']]&.[]('response')
+      liturgy[part.snake_case]['alt'][book['short_title']]['response']
     else
-      week[part.snake_case]['title']
+      liturgy[part.snake_case]['title']
     end
   when /Gospel/
-    week[part.snake_case]['alt'][book['short_title']]['response']
+    liturgy[part.snake_case]['alt'][book['short_title']]['response']
   end
 end
 
-def sub_description(book, part, week)
+def sub_description(book, part, liturgy)
   lines = []
-  if week['liturgy'] =~ / of /
-    lines << "#{book['title']} #{part} for the #{week['liturgy']}"
+  if liturgy['liturgy_name'] =~ / of /
+    lines << "#{book['title']} #{part} for the #{liturgy['liturgy_name']}"
   else
-    lines << "#{book['title']} #{part} for #{week['liturgy']}"
+    lines << "#{book['title']} #{part} for #{liturgy['liturgy_name']}"
   end
 
   if part =~ /Gospel/
-    lines << "#{week['gospel_acclamation']['verse']}"
+    lines << "#{liturgy['gospel_acclamation']['verse']}"
   end
   lines.join("\n")
 end
@@ -60,17 +60,21 @@ def extra
   ].join("\n")
 end
 
-def base_filename(book, part, week)
-  "#{book['short_title']}_#{week['liturgy_short_name']}_#{book['short_publisher']}_#{short_part(part)}"
+def base_filename(book, part, liturgy)
+  "#{book['short_title']}_#{liturgy['liturgy_short_name']}_#{book['short_publisher']}_#{short_part(part)}"
+end
+
+def parts(book, liturgy)
+  liturgy['alternate_parts']&.[](book['short_title']) || book['parts']
 end
 
 data['Liturgies'].each do |liturgy|
   File.open(liturgy['liturgy_short_name']+"descriptions.txt", "wt+") do |f|
     f.puts "=== #{liturgy['liturgy_name']} ==="
     data['Books'].each do |book|
-      book['parts'].each do |part|
+      parts(book, liturgy).each do |part|
         f.puts "Filename: "+ base_filename(book, part, liturgy)
-        f.puts "\"#{title(book, part, liturgy)}\" - #{author(book, part, week)}"
+        f.puts "\"#{title(book, part, liturgy)}\" - #{author(book, part, liturgy)}"
         f.puts sub_description(book, part, liturgy)
         f.puts extra
         f.puts "##{liturgy['liturgy_short_name']}"
